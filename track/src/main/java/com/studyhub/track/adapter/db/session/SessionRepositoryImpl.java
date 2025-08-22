44722a4addc26903b699964172829235acb341d3
Timo Neske <neske.timo93@gmail.com>
2025-08-22 17:41:38 +0200

    update files

--- track/src/main/java/com/studyhub/track/adapter/db/session/SessionRepositoryImpl.java
+++ track/src/main/java/com/studyhub/track/adapter/db/session/SessionRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.studyhub.track.adapter.db.session;
 
 import com.studyhub.track.application.service.SessionRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class SessionRepositoryImpl implements SessionRepository {
 }
--- track/src/main/java/com/studyhub/track/application/service/SessionService.java
+++ track/src/main/java/com/studyhub/track/application/service/SessionService.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class SessionService {
 
+	private final SessionRepository sessionRepository;
+
+	public SessionService(SessionRepository sessionRepository) {
+		this.sessionRepository = sessionRepository;
+	}
+
 	public void save(Session session) {
 		// implement db connection and save logic here
 	}
