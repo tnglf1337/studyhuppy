6dce23d7b0979bace86fa11b35f198fd8a350643
Timo Neske <neske.timo93@gmail.com>
2025-08-23 00:37:58 +0200

    component now fetches all sessions of the user

--- frontend/src/app/modul-service/session/start/start.component.ts
+++ frontend/src/app/modul-service/session/start/start.component.ts
@@ -1,32 +1,41 @@
-import {Component, OnInit} from '@angular/core';
-import {NgIf} from '@angular/common';
+import {Component, inject, OnInit} from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
 import {SessionStateManager} from '../session-state-manager.service';
-import {Block, Session} from '../session-domain';
+import {Session} from '../session-domain';
 import {RouterLink} from '@angular/router';
+import {SessionApiService} from '../session-api.service';
+import {TimeFormatPipe} from '../../module/time-format.pipe';
 
 @Component({
   selector: 'app-session-start',
-  imports: [NgIf, RouterLink],
+  imports: [NgIf, RouterLink, NgForOf, TimeFormatPipe],
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.scss', '../../../general.scss', '../../../button.scss']
 })
 export class SessionStartComponent implements OnInit{
-  session: any // TODO: need to get all configured sessions from backend
+  sessions: any
+  selectedSession!: Session | null;
+  sessionApiService = inject(SessionApiService)
   sessionStateManager!: SessionStateManager;
   sessionStarted = true;
   sessionPaused = false;
 
   ngOnInit(): void {
-    // dummy data!
-    let blocks = []
-    for(let i = 0; i < 3; i++) {
-      const block = new Block(10, 10, i.toString());
-      blocks.push(block)
-    }
-    this.session = new Session("dummy titel", "eine session", blocks);
-    // end dummy data
+    this.sessionApiService.getSessions().subscribe({
+      next: (data) => {
+        if (!data) {
+          console.log("Keine Sessions vorhanden (204 No Content)");
+          this.sessions = null;
+          this.selectedSession = null;
+          return;
+        }
 
-    this.sessionStateManager = new SessionStateManager(this.session)
+        console.log("received sessions from backend", data);
+        this.sessions = data
+        this.selectedSession = this.sessions[0];
+        this.sessionStateManager = new SessionStateManager(this.selectedSession)
+      }
+    })
   }
 
 
