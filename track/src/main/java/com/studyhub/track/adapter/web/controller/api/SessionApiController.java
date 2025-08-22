1c9ef36341763c1b08eb1d00776cf46b3cb03e90
Timo Neske <neske.timo93@gmail.com>
2025-08-22 17:38:32 +0200

    created SessionService.java and added to controller class

--- track/src/main/java/com/studyhub/track/adapter/web/controller/api/SessionApiController.java
+++ track/src/main/java/com/studyhub/track/adapter/web/controller/api/SessionApiController.java
@@ -1,5 +1,6 @@
 package com.studyhub.track.adapter.web.controller.api;
 
+import com.studyhub.track.application.service.SessionService;
 import com.studyhub.track.domain.model.session.Session;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/session/v1")
 public class SessionApiController {
 
+	private final SessionService sessionService;
+
+	public SessionApiController(SessionService sessionService) {
+		this.sessionService = sessionService;
+	}
+
 	@PostMapping("/create")
 	public ResponseEntity<Void> createSession(@RequestBody Session session) {
 		System.out.println("ping session create");
 		System.out.println(session);
 
 		//Todo - db schema erstellen, klassen dafür erstellen und objekt speichern und testen
+		sessionService.save(session);
 
 		return ResponseEntity.ok().build();
 	}
---
+++ track/src/main/java/com/studyhub/track/application/service/SessionService.java
@@ -0,0 +1,13 @@
+package com.studyhub.track.application.service;
+
+import com.studyhub.track.domain.model.session.Session;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SessionService {
+
+	public void save(Session session) {
+		// implement db connection and save logic here
+	}
+
+}
