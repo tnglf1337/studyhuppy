5e0e6b3b61a5b6c6208c8f1c9ae0f7b490e2a908
Timo Neske <neske.timo93@gmail.com>
2025-08-22 09:47:11 +0200

    added lombock annotation to files

--- track/src/main/java/com/studyhub/track/domain/model/session/Block.java
+++ track/src/main/java/com/studyhub/track/domain/model/session/Block.java
@@ -1,7 +1,9 @@
 package com.studyhub.track.domain.model.session;
 
+import lombok.Data;
 import java.util.UUID;
 
+@Data
 public class Block {
 	private UUID id;
 	private int lernzeitSeconds;
--- track/src/main/java/com/studyhub/track/domain/model/session/Session.java
+++ track/src/main/java/com/studyhub/track/domain/model/session/Session.java
@@ -1,8 +1,10 @@
 package com.studyhub.track.domain.model.session;
 
+import lombok.Data;
 import java.util.List;
 import java.util.UUID;
 
+@Data
 public class Session {
 	private UUID id;
 	private String titel;
