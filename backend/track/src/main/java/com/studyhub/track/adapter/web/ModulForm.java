d73ae8e0642073de74db54f93a42300e105434df
Timo Neske <neske.timo93@gmail.com>
2025-11-19 17:47:31 +0100

    Update class

--- backend/track/src/main/java/com/studyhub/track/adapter/web/ModulForm.java
+++ backend/track/src/main/java/com/studyhub/track/adapter/web/ModulForm.java
@@ -14,34 +14,10 @@ public record ModulForm(
 		Integer selbststudiumStunden,
 		LocalDate klausurDatum,
 		String time
-
-		/** Für Stapel anlegen
-		 Boolean stapelCheckbox,
-		 String stapelName,
-		 String beschreibung,
-		 String lernstufen,
-
-		 Für Semester
-		 Boolean semesterCheckbox,
-		 LocalDate semesterBeginn,
-		 LocalDate semesterEnde,
-		 LocalDate vlBeginn,
-		 LocalDate vlEnde)
-		 **/
 )
 {
 	public Modul newModulFromFormData(ModulForm modulForm, String username, int semester) {
 		Kreditpunkte kreditpunkte = new Kreditpunkte(modulForm.creditPoints(), modulForm.kontaktzeitStunden(), modulForm.selbststudiumStunden());
-		//TODO: Semester objekt erstellen
-		return new Modul(UUID.randomUUID(), name, 0, kreditpunkte, username, true, semester, new Semester(), null);
-	}
-
-	public SemesterTyp decideSemesterTyp(LocalDate semesterBeginn) {
-		if(semesterBeginn.isAfter(LocalDate.of(LocalDate.now().getYear(), 2, 1)) && semesterBeginn.isBefore(LocalDate.of(LocalDate.now().getYear(), 8, 1))) {
-			return SemesterTyp.SOMMERSEMESTER;
-		} else if(semesterBeginn.isAfter(LocalDate.of(LocalDate.now().getYear(), 8, 1)) && semesterBeginn.isBefore(LocalDate.of(LocalDate.now().getYear()+1, 2, 1))) {
-			return SemesterTyp.WINTERSEMESTER;
-		}
-		return null;
+		return new Modul(UUID.randomUUID(), name, 0, kreditpunkte, username, true, semester, null, null);
 	}
 }
