e3175d5a31e647d63c1a06757b7f733a44991890
Timo Neske <neske.timo93@gmail.com>
2025-11-19 21:03:57 +0100

    Mark as deprecated

--- backend/track/src/main/java/com/studyhub/track/adapter/web/controller/request/dto/AddTimeRequest.java
+++ backend/track/src/main/java/com/studyhub/track/adapter/web/controller/request/dto/AddTimeRequest.java
@@ -3,6 +3,7 @@ package com.studyhub.track.adapter.web.controller.request.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@Deprecated
 @Data
 @AllArgsConstructor
 public class AddTimeRequest {
--- backend/track/src/main/java/com/studyhub/track/adapter/web/controller/request/dto/ModulRequest.java
+++ backend/track/src/main/java/com/studyhub/track/adapter/web/controller/request/dto/ModulRequest.java
@@ -1,5 +1,6 @@
 package com.studyhub.track.adapter.web.controller.request.dto;
 
+@Deprecated
 public record ModulRequest(
 		String modulName,
 		Integer creditPoints,
