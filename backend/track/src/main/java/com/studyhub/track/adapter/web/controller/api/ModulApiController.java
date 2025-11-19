4aa8b2e39b1239b2d747533e6a835e50f764c5a1
Timo Neske <neske.timo93@gmail.com>
2025-11-19 21:12:34 +0100

    restructure file

--- backend/track/src/main/java/com/studyhub/track/adapter/web/controller/api/ModulApiController.java
+++ backend/track/src/main/java/com/studyhub/track/adapter/web/controller/api/ModulApiController.java
@@ -16,7 +16,6 @@ import com.studyhub.track.application.service.dto.NeuerModulterminRequest;
 import com.studyhub.track.domain.model.modul.Modul;
 import com.studyhub.track.domain.model.modul.Modultermin;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -43,6 +42,93 @@ public class ModulApiController {
         this.jwtService = jwtService;
         this.metrics = metrics;
     }
+
+	/** GET MAPPINGS **/
+	@GetMapping("/get-seconds")
+	public ResponseEntity<Integer> getSeconds(@RequestParam("fachId") String fachId) {
+		return ResponseEntity.ok(modulService.findSecondsById(UUID.fromString(fachId)));
+	}
+
+	@GetMapping("/module-map")
+	public ResponseEntity<Map<UUID, String>> getModuleMap(HttpServletRequest req) {
+		String username = jwtService.extractUsernameFromHeader(req);
+		return ResponseEntity.ok(modulService.getModuleMap(username));
+	}
+
+	@GetMapping("/module-name")
+	public ResponseEntity<String> getModulName(String modulFachId) {
+		String modulName = modulService.findModulNameByFachId(UUID.fromString(modulFachId));
+		return ResponseEntity.ok(modulName);
+	}
+
+	@GetMapping("/get-all-module-by-fachsemester")
+	public ResponseEntity<Map<Integer, List<Modul>>> getAllModuleByFachsemester(HttpServletRequest request) {
+		String username = jwtService.extractUsernameFromHeader(request);
+		Map<Integer, List<Modul>> resultMap = modulService.getFachsemesterModuleMap(username);
+		return ResponseEntity.ok(resultMap);
+	}
+
+	@GetMapping("/get-modul-select-data")
+	public ResponseEntity<List<ModulSelectDto>> getModulSelectData(HttpServletRequest request) {
+		String username = jwtService.extractUsernameFromHeader(request);
+		List<ModulSelectDto> data = modulService.getModulSelectData(username);
+		return ResponseEntity.ok(data);
+	}
+
+	@GetMapping("/has-module")
+	public ResponseEntity<Boolean> hasModule(HttpServletRequest request) {
+		String username = jwtService.extractUsernameFromHeader(request);
+		List<Modul> data = modulService.findAllByUsername(username);
+		return ResponseEntity.ok(!data.isEmpty());
+	}
+
+	@GetMapping("/get-aktivitaet-status/{fachId}")
+	public ResponseEntity<Boolean> getAktivitaetStatus(@PathVariable("fachId") String fachId) {
+		boolean isActive = modulService.findByFachId(UUID.fromString(fachId)).isActive();
+		return ResponseEntity.ok(isActive);
+	}
+
+	@GetMapping("/getModultermine")
+	public ResponseEntity<List<Modultermin>> getModultermine(@RequestParam("modulId") UUID modulId) {
+		return ResponseEntity.ok(modulService.getModultermineByModulId(modulId));
+	}
+
+	@GetMapping("/get-all-by-username")
+	public ResponseEntity<List<ModulDto>> getAllByUsername(HttpServletRequest request) {
+		String username = jwtService.extractUsernameFromHeader(request);
+		List<ModulDto> l = modulService.findAllByUsername(username).stream().map(ModulMapper::toModulDtoNoId).toList();
+		return ResponseEntity.ok(l);
+	}
+
+	@GetMapping("/get-active-modules")
+	public ResponseEntity<List<Modul>> getActiveModules(HttpServletRequest request) {
+		String username = jwtService.extractUsernameFromHeader(request);
+		List<Modul> l = modulService.findActiveModuleByUsername(true, username);
+		return ResponseEntity.ok(l);
+	}
+
+	/** POST MAPPINGS **/
+	@PostMapping("/new-modul")
+	public ResponseEntity<Void> newModule(@RequestBody ModulForm modulForm, HttpServletRequest request) {
+		String username = jwtService.extractUsernameFromHeader(request);
+		int semester = authenticationService.getSemesterOfUser(username, jwtService.extractTokenFromHeader(request.getHeader("Authorization")));
+		Modul modul = modulForm.newModulFromFormData(username, semester);
+		modulService.saveNewModul(modul);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+
+	}
+
+	@PutMapping("/change-active")
+	public ResponseEntity<Void> activate(@RequestParam("fachId")  String fachId) {
+		modulService.toggleModulActivity(UUID.fromString(fachId));
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+
+	@PostMapping("/addModultermin")
+	public ResponseEntity<Void> addModultermin(@RequestBody NeuerModulterminRequest req) {
+		modulService.saveNewModultermin(req);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
 
 	@PostMapping("/update")
 	public ResponseEntity<Void> updateSeconds(@RequestBody TimerRequest timerRequest, HttpServletRequest httpServletRequest) {
@@ -73,33 +159,18 @@ public class ModulApiController {
 		}
 	}
 
-	@GetMapping("/get-seconds")
-	public ResponseEntity<Integer> getSeconds(@RequestParam("fachId") String fachId) {
-		return ResponseEntity.ok(modulService.getSecondsForId(UUID.fromString(fachId)));
-	}
-
-	@GetMapping("/module-map")
-	public ResponseEntity<Map<UUID, String>> getModuleMap(HttpServletRequest req) {
-		String username = jwtService.extractUsernameFromHeader(req);
-		return ResponseEntity.ok(modulService.getModuleMap(username));
-	}
-
-	@GetMapping("/module-name")
-	public ResponseEntity<String> getModulName(String modulFachId) {
-		String modulName = modulService.findModulNameByFachId(UUID.fromString(modulFachId));
-		return ResponseEntity.ok(modulName);
-	}
-
+	/** PUT MAPPINGS **/
 	@PutMapping("/reset")
 	public ResponseEntity<Void> reset(@RequestParam("fachId") String fachId) {
 		try {
 			modulService.resetSecondsLearnedOfModul(UUID.fromString(fachId));
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-        return ResponseEntity.status(HttpStatus.OK).build();
+		}
+		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 
+	/** DELETE MAPPINGS **/
 	@DeleteMapping("/delete")
 	public ResponseEntity<Void> deleteModul(@RequestParam UUID fachId, HttpServletRequest httpServletRequest) {
 		String username = jwtService.extractUsernameFromHeader(httpServletRequest);
@@ -112,71 +183,4 @@ public class ModulApiController {
 		}
 	}
 
-	@GetMapping("/get-active-modules")
-	public ResponseEntity<List<Modul>> getActiveModules(HttpServletRequest request) {
-		String username = jwtService.extractUsernameFromHeader(request);
-		List<Modul> l = modulService.findActiveModuleByUsername(true, username);
-		return ResponseEntity.ok(l);
-	}
-
-	@PostMapping("/new-modul")
-	public ResponseEntity<Void> newModule(@RequestBody ModulForm modulForm, HttpServletRequest request) {
-		String username = jwtService.extractUsernameFromHeader(request);
-		int semester = authenticationService.getSemesterOfUser(username, jwtService.extractTokenFromHeader(request.getHeader("Authorization")));
-		Modul modul = modulForm.newModulFromFormData(username, semester);
-		modulService.saveNewModul(modul);
-		return ResponseEntity.status(HttpStatus.CREATED).build();
-
-	}
-
-	@GetMapping("/get-all-by-username")
-	public ResponseEntity<List<ModulDto>> getAllByUsername(HttpServletRequest request) {
-		String username = jwtService.extractUsernameFromHeader(request);
-		List<ModulDto> l = modulService.findAllByUsername(username).stream().map(ModulMapper::toModulDtoNoId).toList();
-		return ResponseEntity.ok(l);
-	}
-
-	@PutMapping("/change-active")
-	public ResponseEntity<Void> activate(@RequestParam("fachId")  String fachId) {
-		modulService.toggleModulActivity(UUID.fromString(fachId));
-		return ResponseEntity.status(HttpStatus.OK).build();
-	}
-
-	@GetMapping("/get-aktivitaet-status/{fachId}")
-	public ResponseEntity<Boolean> getAktivitaetStatus(@PathVariable("fachId") String fachId) {
-		boolean isActive = modulService.findByFachId(UUID.fromString(fachId)).isActive();
-		return ResponseEntity.ok(isActive);
-	}
-
-	@GetMapping("/getModultermine")
-	public ResponseEntity<List<Modultermin>> getModultermine(@RequestParam("modulId") UUID modulId) {
-		return ResponseEntity.ok(modulService.getModultermineByModulId(modulId));
-	}
-
-	@PostMapping("/addModultermin")
-	public ResponseEntity<Void> addModultermin(@RequestBody NeuerModulterminRequest req) {
-		modulService.saveNewModultermin(req);
-		return ResponseEntity.status(HttpStatus.CREATED).build();
-	}
-
-	@GetMapping("/get-all-module-by-fachsemester")
-	public ResponseEntity<Map<Integer, List<Modul>>> getAllModuleByFachsemester(HttpServletRequest request) {
-		String username = jwtService.extractUsernameFromHeader(request);
-		Map<Integer, List<Modul>> resultMap = modulService.getFachsemesterModuleMap(username);
-		return ResponseEntity.ok(resultMap);
-	}
-
-	@GetMapping("/get-modul-select-data")
-	public ResponseEntity<List<ModulSelectDto>> getModulSelectData(HttpServletRequest request) {
-		String username = jwtService.extractUsernameFromHeader(request);
-		List<ModulSelectDto> data = modulService.getModulSelectData(username);
-		return ResponseEntity.ok(data);
-	}
-
-	@GetMapping("/has-module")
-	public ResponseEntity<Boolean> hasModule(HttpServletRequest request) {
-		String username = jwtService.extractUsernameFromHeader(request);
-		List<Modul> data = modulService.findAllByUsername(username);
-		return ResponseEntity.ok(!data.isEmpty());
-	}
 }
